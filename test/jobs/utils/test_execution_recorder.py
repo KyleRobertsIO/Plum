04cfb95e2c975cde167e76d31ec2202ee120e169
Kyle Roberts <kkroberts1635@gmail.com>
2024-05-05 01:21:57 +0000

    test(test/jobs/utils/test_execution_recorder.py): Updated tests using recorder fixture to use a deepcopyto avoid referencing the same object at runtime

--- test/jobs/utils/test_execution_recorder.py
+++ test/jobs/utils/test_execution_recorder.py
@@ -1,4 +1,5 @@
 import pytest
+import copy
 
 from typing import Optional
 
@@ -17,25 +18,29 @@ def create_execution_recorder():
 def test_set_start_timestamp(
     create_execution_recorder: ExecutionRecorder
 ):
-    reset_err: Optional[StartTimestampResetError] = create_execution_recorder.set_start_timestamp()
-    assert reset_err == None, 'expected None returned due to first time setting timestamp but got exception'
-    assert create_execution_recorder.start_timestamp != None, 'expected datetime to be set on "start_timestamp" but was found to be None'
+    recorder: ExecutionRecorder = copy.deepcopy(create_execution_recorder)
 
-    reset_err: Optional[StartTimestampResetError] = create_execution_recorder.set_start_timestamp()
+    reset_err: Optional[StartTimestampResetError] = recorder.set_start_timestamp()
+    assert reset_err == None, 'expected None returned due to first time setting timestamp but got exception'
+    assert recorder.start_timestamp != None, 'expected datetime to be set on "start_timestamp" but was found to be None'
+
+    reset_err: Optional[StartTimestampResetError] = recorder.set_start_timestamp()
     assert isinstance(reset_err, StartTimestampResetError), 'expected "StartTimestampResetError" due to second run of class method but got None'
 
 @pytest.mark.unit
 def test_set_end_timestamp(
     create_execution_recorder: ExecutionRecorder
 ):
-    err: PrematureEndTimestampError = create_execution_recorder.set_end_timestamp()
-    assert isinstance(err, PrematureEndTimestampError), 'expected PrematureEndTimestampError, to be returned due to missing "start_timestamp"'
+    recorder: ExecutionRecorder = copy.deepcopy(create_execution_recorder)
 
-    reset_err: Optional[StartTimestampResetError] = create_execution_recorder.set_start_timestamp()
+    err: PrematureEndTimestampError = recorder.set_end_timestamp()
+    assert isinstance(err, PrematureEndTimestampError), f'expected PrematureEndTimestampError, to be returned due to missing "start_timestamp"'
+
+    reset_err: Optional[StartTimestampResetError] = recorder.set_start_timestamp()
     assert reset_err == None, 'expected None returned due to first time setting timestamp but got exception'
     
-    err: Optional[PrematureEndTimestampError] = create_execution_recorder.set_end_timestamp()
+    err: Optional[PrematureEndTimestampError] = recorder.set_end_timestamp()
     assert err == None, 'expected no PrematureEndTimestampError to be returned, due to "start_timestamp" being provided'
 
-    reset_err: Optional[StartTimestampResetError] = create_execution_recorder.set_end_timestamp()
+    reset_err: Optional[StartTimestampResetError] = recorder.set_end_timestamp()
     assert isinstance(reset_err, EndTimestampResetError), 'expected "EndTimestampResetError" due to second run of class method but got None'
